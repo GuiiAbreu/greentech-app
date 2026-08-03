8170b7bb8d94add79aed6d191c987a607589101a
Guilherme Henrique <guilherme.pessoa@academico.ifpb.edu.br>
2026-08-03 15:17:14 -0300

    refactor(api): update prisma client setup

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,10 +1,12 @@
-import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config"; 
+import { PrismaPg } from "@prisma/adapter-pg"; 
+import Prisma from "@prisma/client"; 
 
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) throw new Error("DATABASE_URL is missing");
+const { PrismaClient } = Prisma; 
 
-const adapter = new PrismaPg({ connectionString });
+const connectionString = process.env.DATABASE_URL; 
+    if (!connectionString) throw new Error("DATABASE_URL is missing"); 
 
+    const adapter = new PrismaPg({ connectionString }); 
+    
 export const prisma = new PrismaClient({ adapter });
